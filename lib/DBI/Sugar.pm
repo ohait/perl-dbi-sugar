fd1e7da38e13bc59aa36aacd38e7ef869e948192
Francesco Rivetti <oha@opera.com>
2014-08-12 06:53:45 +0000

    make stm --DBI comment refer to the proper caller

--- lib/DBI/Sugar.pm
+++ lib/DBI/Sugar.pm
@@ -384,7 +384,13 @@ IMPORTANT: it will die if more than one rows are found.
 =cut
 
 sub SELECT_ROW($$) {
-    _SELECT_ROW(@_, sub {});
+    my ($stm, $binds) = @_;
+    my $out;
+    _SELECT($stm, $binds, sub {
+            die "expected 1 row, got more: $stm" if $out;
+            $out = {%_};
+        }, sub {});
+    return $out ? %$out : ();
 }
 
 sub _SELECT_ROW {
